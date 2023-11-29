d264e20b109552dd7c73f9797708a31fb9341acd
janishareyya <147384025+janishareyya@users.noreply.github.com>
2023-11-29 15:51:04 +0530

    Add files via upload
    Assignment-1

---
+++ asignment/app.js
@@ -0,0 +1,4 @@
+var parent=[React.createElement("h1",{},"HELLO WORLD"),React.createElement("h2",{},"WELCOME TO REACT"),React.createElement("h3",{},"using js")]
+var htmlRoot=document.getElementById("root")
+var rootReact=ReactDOM.createRoot(htmlRoot)
+rootReact.render(parent)
